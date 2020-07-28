91cb222040f663c03c42f303a44fee2ec1a72c6b
DiogodePaula <depaulatrabalhos@gmail.com>
2020-07-28 14:28:26 -0300

    ok

--- src/database/migrations/20200723201631-create-table-users.js
+++ src/database/migrations/20200723201631-create-table-users.js
@@ -24,6 +24,10 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
